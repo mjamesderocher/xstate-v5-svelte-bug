efd8d3c52a772c2a47f3011bfe2ba7f5fdd59d2d
mjamesderocher <mjamesderocher@gmail.com>
2023-07-26 08:12:40 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createMachine, interpret } from "xstate";
+
+  const machine = createMachine({
+    initial: "start",
+    states: {
+      start: {
+        after: {
+          // 500: {
+          // }
+        }
+      }
+    }
+  });
+
+  const myActor = interpret(machine).start();
+</script>
+
+<main>
+  {JSON.stringify($myActor)}
+</main>
